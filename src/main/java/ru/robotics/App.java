9a46f72634195136177bdba6975f7303cbf35624
Dmitry Pyshkin <pyshkindv@bcs.ru>
2020-02-10 02:21:44 +0700

    Typo fix

--- src/main/java/ru/robotics/App.java
+++ src/main/java/ru/robotics/App.java
@@ -14,23 +14,15 @@ import ru.robotics.equipment.Kevlar;
 import ru.robotics.equipment.SpecialBullet;
 import ru.robotics.factories.actors.ActorFactory;
 import ru.robotics.factories.referess.RefereeFactory;
-import ru.robotics.referees.SimpleReferee;
 import ru.robotics.weapons.Gun;
 
 public class App {
 
   public static void main(String[] args) {
-    SimpleReferee referee = RefereeFactory.createSimpleReferee(List.of(
+    RefereeFactory.createSimpleReferee(List.of(
         ActorFactory.createKiller("killer1", List.of(new Gun<>(List.of(new SpecialBullet())))),
         ActorFactory.createProtectEquippedKiller("killer2", List.of(new Gun<>(List.of())), List.of(new Kevlar())))
-     /* ActorFactory.createKiller("killer1", List.of(new Gun<>(List.of(new SpecialBullet())))),
-      ActorFactory.createKiller("killer2", List.of(new Gun<>(List.of()))),
-      ActorFactory.createKiller("killer3", List.of(new Automat<>(List.of(new SpecialBullet())))),
-      ActorFactory.createProtectEquippedKiller("killer4", List.of(new Automat<>(List.of())), List.of(new Kevlar())))*/
-    );
-    referee.getWinner();
-    referee.observe();
-    referee.getWinner();
+    ).getWinner();
   }
 
 }
